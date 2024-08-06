1fc22140436198f627b8b4396e36632e80c04e07
Dineshcodesss <imdineshchandru@gmail.com>
2024-08-06 14:23:34 +0530

    project

---
+++ src/Shape.java
@@ -0,0 +1,17 @@
+package com.tnsif.dayeight.abstraction;
+
+//abstract class
+public abstract class Shape {
+     protected float area;
+     
+     //abstract method 
+	 abstract void calArea();
+	 
+	 //concrete method
+	 void show()
+	 {
+		 System.out.println("Area of shape is "+area);
+	 }
+}
+	 
+
